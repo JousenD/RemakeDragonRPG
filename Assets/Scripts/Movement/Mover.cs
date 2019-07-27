24240e1be96775d7ee1fc371510369bd56b8ca5e
Jousen-PC\Jousen <Romero87_2112@hotmail.com>
2019-07-27 18:34:05 +0200

    End Section 2 - Movement

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    Transform target;
+    Ray lastRay;
+
+    // Update is called once per frame
+    void Update()
+    {    
+        if (Input.GetMouseButton(0)){
+            MoveToCursor();
+        }
+        // Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        UpdateAnimator();        
+    }
+
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        lastRay = ray;
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("ForwardSpeed",speed);
+    }
+}
