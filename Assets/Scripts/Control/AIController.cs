62091340e27a4e245ac4d64d9f5ddbcc8863f622
Jousen-PC\Jousen <Romero87_2112@hotmail.com>
2019-09-09 23:57:12 +0200

    Chase Radius implemented

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    [SerializeField] float chaseDistance = 5f;
+
+    private void Update()
+    {
+        if(DistanceToPlayer()<chaseDistance)
+        {
+            print(gameObject.name + " Should chase");
+        }
+
+    }
+
+    private float DistanceToPlayer()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        return Vector3.Distance(transform.position, player.transform.position);
+    }
+}
+
