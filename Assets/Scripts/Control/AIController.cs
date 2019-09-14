a0225ac4fd985223817d30f06af2778a71ade45d
Jousen-PC\Jousen <Romero87_2112@hotmail.com>
2019-09-14 22:58:14 +0200

    Suspicion Behaviour

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -4,38 +4,79 @@ using UnityEngine;
 using RPG.Combat;
 using RPG.Control;
 using RPG.Core;
+using RPG.Movement;
 
 public class AIController : MonoBehaviour
 {
     [SerializeField] float chaseDistance = 5f;
+    [SerializeField] float suspicionTime = 3f;
 
     Fighter fighter;
     Health health;
+    Mover mover;
     GameObject player;
+
+    Vector3 guardPosition;
+    
+    
+    float timeSinceLastSawPlayer = Mathf.Infinity;
 
     private void Start(){
         fighter = GetComponent<Fighter>();
         health = GetComponent<Health>();
+        mover = GetComponent<Mover>();
         player = GameObject.FindWithTag("Player");
+        
+        guardPosition = transform.position;
     }
 
     private void Update()
     {
         if(health.IsDead()) return;
+
         if(InAttackRangeOfPlayer() && fighter.CanAttack(player))
         {
-            GetComponent<Fighter>().Attack(player);
+            timeSinceLastSawPlayer = 0;
+            AttackBehaviour();
         }
-        else{
-            fighter.Cancel();
+        else if (timeSinceLastSawPlayer<suspicionTime)
+        {
+            SuspicionBehaviour();
+        }
+        else
+        {
+            GuardBehaviour();
         }
 
+        timeSinceLastSawPlayer += Time.deltaTime;
+
+    }
+
+    private void GuardBehaviour()
+    {
+        mover.StartMoveAction(guardPosition);
+    }
+
+    private void SuspicionBehaviour()
+    {
+        GetComponent<ActionScheduler>().CancelCurrentAction();
+    }
+
+    private void AttackBehaviour()
+    {
+        GetComponent<Fighter>().Attack(player);
     }
 
     private bool InAttackRangeOfPlayer()
     {
         float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
         return distanceToPlayer < chaseDistance;
+    }
+
+    //Called by Unity
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, chaseDistance);
     }
 }
 
