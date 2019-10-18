b4689ddfbf17ed5a991d0f60bd8747eadbe2350c
Jousen-PC\Jousen <Romero87_2112@hotmail.com>
2019-10-18 17:52:37 +0200

    Arrows with a trail dealing damage

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -9,8 +9,8 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float speed = 1f;
-        
         Health target = null;
+        float damage =0;
 
         // Update is called once per frame
         void Update()
@@ -21,9 +21,16 @@ namespace RPG.Combat
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
-        public void SetTarget(Health target)
+        public void SetTarget(Health target, float damage)
         {
             this.target = target;
+            this.damage = damage;
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            if(other.gameObject.GetComponent<Health>() != target) return;
+            target.TakeDamage(damage);
+            Destroy(gameObject);
         }
 
         private Vector3 GetAimPosition()
